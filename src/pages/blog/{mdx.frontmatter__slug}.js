9f0e538ae4950132dfe9aedfe5635070e1312e14
Nischal Shakya <nischalshakya2015@gmail.com>
2022-11-08 17:14:07 +0545

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,25 @@
 import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Seo from "../../components/seo";
 
 function BlogPost({ data, children }) {
+  const image = getImage(data.mdx.frontmatter.hero_image);
   return (
     <Layout pageTitle='Super Cool Blog Posts'>
       <p>{data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a
+          href={data.mdx.frontmatter.hero_image_credit_link}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       {children}
     </Layout>
   );
@@ -16,8 +29,17 @@ export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
       frontmatter {
+        date
+        slug
         title
-        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(formats: [AUTO, WEBP, AVIF])
+          }
+        }
       }
     }
   }
