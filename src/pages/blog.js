c96ea195986165a699625797f3ff4dd8b0a7b550
Nischal Shakya <nischalshakya2015@gmail.com>
2022-11-08 09:48:24 +0545

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,29 +2,52 @@ import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/seo";
+import PropTypes from "prop-types";
 
 function BlogPage({ data }) {
   return (
     <Layout pageTitle='My Blog Posts'>
-      <ul>
-        {data.allFile.nodes.map(({ name }) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map(({ frontmatter, excerpt, id }) => (
+        <article key={id}>
+          <h2>{frontmatter.title}</h2>
+          <p>Posted: {frontmatter.date}</p>
+          <p>{excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 }
 
-export default BlogPage;
-
 export const query = graphql`
   query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+    allMdx(sort: { order: DESC, fields: frontmatter___date }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
 `;
 
 export const Head = () => <Seo title='My Blog Posts' />;
+
+BlogPage.propTypes = {
+  allMdx: PropTypes.shape({
+    nodes: PropTypes.arrayOf(
+      PropTypes.shape({
+        frontmatter: PropTypes.shape({
+          date: PropTypes.string.isRequired,
+          title: PropTypes.string.isRequired,
+        }),
+        excerpt: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
+      })
+    ),
+  }),
+};
+
+export default BlogPage;
